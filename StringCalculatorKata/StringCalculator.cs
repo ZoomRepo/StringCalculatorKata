d09099215c0ae7f00552028dc82df029dbe9ebe6
ZoomRepo <ollybeumkes@hotmail.co.uk>
2022-03-22 12:11:13 +0000

    Add returns sum of intergers provided in *numbers*

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -8,9 +8,14 @@ namespace StringCalculatorKata
 {
     public class StringCalculator
     {
-        public object Add(string name)
+        public object Add(string numbers)
         {
-            return 0;
+            if (String.IsNullOrEmpty(numbers)) return 0;
+            
+            var result = numbers.Split(',')
+                .Select(s => int.Parse(s));
+
+            return result.Sum();
         }
 
     }
--- StringCalculatorKata/StringCalculator_Add.cs
+++ StringCalculatorKata/StringCalculator_Add.cs
@@ -16,5 +16,35 @@ namespace StringCalculatorKata
             //Assert
             Assert.Equal(0, result);
         }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("5", 5)]
+
+        public void GivenXStringReturnsX(string inputParam, int expectedResult)
+        {
+            //Arrange
+            var calc = new StringCalculator();
+
+            //Act
+            var result = calc.Add(inputParam);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        public void GivenTwoNumbersReturnsSum(string inputParam, int expectedResult)
+        {
+            //Arrange
+            var calculator = new StringCalculator();
+
+            //Act
+            var result = calculator.Add(inputParam);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
     }
 }
